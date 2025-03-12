9f5a3b6570ddada4cae96c1d84cdff3188e8b6a8
RandomEF <71469308+RandomEF@users.noreply.github.com>
2025-03-12 16:04:41 +0000

    changed achievement columns

--- BacklogCreator.py
+++ BacklogCreator.py
@@ -15,7 +15,7 @@ def GetSteamLibrary():
     gamesList = []
     gamesResponse = response["response"]["games"]
     for i in range(response["response"]["game_count"]):
-        gamesList.append([gamesResponse[i]["appid"], gamesResponse[i]["name"], imageURL + str(gamesResponse[i]["appid"]) + "/" + gamesResponse[i]["img_icon_url"] + ".jpg",  gamesResponse[i]["playtime_forever"], 0, 0, 0, 0])
+        gamesList.append([gamesResponse[i]["appid"], gamesResponse[i]["name"], imageURL + str(gamesResponse[i]["appid"]) + "/" + gamesResponse[i]["img_icon_url"] + ".jpg",  gamesResponse[i]["playtime_forever"], 0, 0, 0, 0, 0, 0])
     return gamesList
 
 def GetAchievementPercentage(game):
@@ -31,7 +31,7 @@ def GetAchievementPercentage(game):
         for achievement in response["achievements"]:
             if achievement["achieved"] == 1:
                 achieved += 1
-        return str(achieved) + "/" + str(len(response["achievements"])) 
+        return achieved, len(response["achievements"]), round(achieved*100/len(response["achievements"]), 2)
 
 def FillGameData(library):
     exceptions = []
@@ -43,7 +43,7 @@ def FillGameData(library):
             game[4] = best.main_story
             game[5] = best.main_extra
             game[6] = best.completionist
-            game[7] = GetAchievementPercentage(game[0])
+            game[7], game[8], game[9] = GetAchievementPercentage(game[0])
         else:
             exceptions.append(game)
     return exceptions
@@ -51,13 +51,13 @@ def FillGameData(library):
 def WriteCSV(library):
     with open("backlog.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["App ID", "Name", "Image URL", "Minutes of Playtime", "Main", "Main + Extra", "Completionist", "Achievements"])
+        writer.writerow(["App ID", "Name", "Image URL", "Minutes of Playtime", "Main", "Main + Extra", "Completionist", "Achievements completed", "Achievements total", "Percentage"])
         writer.writerows(library)
 
 def ValidateInput(type):
     passed = False
     while not passed:
-        inp = input(f"Input {type} time: ")
+        inp = input(type)
         if inp == "NA":
             return inp
         try:
@@ -78,26 +78,49 @@ def FillFile():
             game[4] = best.main_story
             game[5] = best.main_extra
             game[6] = best.completionist
-            game[7] = GetAchievementPercentage(game[0])
+            game[7], game[8], game[9] = GetAchievementPercentage(game[0])
         else:
             print(game)
-            game[4] = ValidateInput("Main")
-            game[5] = ValidateInput("Main + Extra")
-            game[6] = ValidateInput("Completionist")
+            game[4] = ValidateInput("Input Main time:")
+            game[5] = ValidateInput("Input Main + Extra time:")
+            game[6] = ValidateInput("Input Completionist time:")
             print("NA at any step to set amount of achievements to NA.")
-            achieved = input("Please enter the amount of achievements achieved: ")
+            achieved = ValidateInput("Please enter the amount of achievements achieved: ")
             if achieved == "NA":
                 game[7] = "NA"
+                game[8] = "NA"
+                game[9] = "NA"
                 continue
-            total = input("Please enter the amount of achievements achieved: ")
+            total = ValidateInput("Please enter the amount of achievements achieved: ")
             if total == "NA":
                 game[7] = "NA"
+                game[8] = "NA"
+                game[9] = "NA"
                 continue
-            game[7] = str(achieved) + "/" + str(total)
+            game[7] = int(achieved)
+            game[8] = int(total)
+            game[9] = round(achieved*100/total, 2)
+
     WriteCSV(library)
 
 def main():
-    FillFile()
+    #FillFile()
+    headers = []
+    rows = []
+    with open("backlog.csv", "r") as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        for row in reader:
+            if row[9] != "NA":
+                row[9] = round(int(row[7])*100/int(row[8]), 2)
+
+            rows.append(row)
+    
+    with open("backlog.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(rows)
+
 
 if __name__ == '__main__':
     main()
